a165f130cbf8b61615b6c6c887723f896d3deaf2
Leonid Varsaminov <leonid.varsaminov@accedia.com>
2024-04-05 14:58:07 +0300

    Added product.cs to models and set its properties.

---
+++ SportsGoods.Core/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsGoods.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string ProductCategory { get; set; } = string.Empty; 
+
+    }
+}
